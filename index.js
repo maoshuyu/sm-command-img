80a155aad5de8b9446dee87f20f1d1f40158ad98
maoshuyu <maoshuyu@gmail.com>
2014-12-17 11:33:34 +0800

    sm-commend-img init

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var optimizer = require('./lib/optimizer');
+
+exports.name = 'img';
+exports.usage = '<command> [options]';
+exports.desc = 'optimizer img';
+exports.register = function(commander) {
+    commander
+        .option('-e, --engine <name>', 'compress engine', String, 'tinypng')
+        .action(function() {
+            var options = arguments[arguments.length - 1];
+
+            if (options.verbose) {
+                fis.log.level = fis.log.L_ALL;
+            }
+
+            var root = fis.util.realpath(options.root || process.cwd());
+
+            // init project
+            fis.project.setProjectRoot(root);
+
+            optimizer(root, options);
+        });
+    
+};
